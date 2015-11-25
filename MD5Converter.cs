6df8df47357bb58293cfdf2dc515f7f234f04f78
Serkan Sendur <serkan_sendur@hotmail.com>
2015-11-25 19:36:05 +0200

    MD5Converter added.

---
+++ MD5Converter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerqUtil.Converters
+{
+    public class MD5Converter
+    {
+        public static string ConvertToMD5(string text)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+            }
+        }
+
+        public static bool VerifyMD5Hash(string plainText,string hash)
+        {
+            // Hash the input.
+            string hashOfInput = ConvertToMD5(plainText);
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SerqUtil.Converters.Tests/MD5ConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerqUtil.Converters.Tests
+{
+    [TestClass]
+    public class MD5ConverterTests
+    {
+        [TestMethod]
+        public void ConvertToMD5()
+        {
+            string hash = MD5Converter.ConvertToMD5("123456");
+        }
+        [TestMethod]
+        public void VerifyMD5Hash()
+        {
+            bool verified = MD5Converter.VerifyMD5Hash("123456", "e10adc3949ba59abbe56e057f20f883e");
+        }
+    }
+}
